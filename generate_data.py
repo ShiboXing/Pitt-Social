fd4a008c2fe3d682c6152e0f8590fd56afec92bd
ShiboXing <shx26@pitt.edu>
2019-11-05 15:20:39 -0500

    Create generate_data.py

---
+++ generate_data.py
@@ -0,0 +1,19 @@
+for i in range(100):
+    user_id=i
+    fname=''
+    lname=''
+    email_name=''
+    password=''
+    for j in range(randint(3,10))
+        fname+=chr(randint(97,97+26))
+        lname+=chr(randint(97,97+26))
+        email_name+=chr(randint(97,97+26))
+        password+=chr(randint(97,97+26))\
+    year=randint(1990,2019)
+    month=randint(1,12)
+    day=randint(1,30)
+
+    query='insert into profile'
+    
+
+    print()
