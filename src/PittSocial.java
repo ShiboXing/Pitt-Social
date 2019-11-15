874fd7fec4de7543535a5daf20aba243dceea40f
ShiboXing <shx26@pitt.edu>
2019-11-15 00:52:09 -0500

    added postgresql.jar in dependencies

--- src/PittSocial.java
+++ src/PittSocial.java
@@ -1,6 +1,10 @@
-public class PittSocial {
-    public static void main(String args[])
-    {
+import java.util.Properties;
+import java.sql.*;
 
+public class PittSocial {
+    public static void main(String args[]) throws ClassNotFoundException
+    {
+        Class c=Class.forName("org.postgresql.Driver");
+        
     }
 }
