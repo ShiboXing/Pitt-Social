764c8da7dadb6dd81435c0b21fb5c19e7804052e
ShiboXing <shx26@pitt.edu>
2019-11-15 12:21:19 -0500

    added connection

--- src/PittSocial.java
+++ src/PittSocial.java
@@ -2,11 +2,12 @@ import java.util.Properties;
 import java.sql.*;
 
 public class PittSocial {
-    public static void main(String args[]) throws ClassNotFoundException
-    {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException {
         Class c=Class.forName("org.postgresql.Driver");
         Properties props =new Properties();
+        String url="jdbc:postgresql://localhost/postgres";
         props.setProperty("user","postgres");
-        props.setProperty("password","password");
+        props.setProperty("password","team3");
+        Connection conn =DriverManager.getConnection(url,props);
     }
 }
