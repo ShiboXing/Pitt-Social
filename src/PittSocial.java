26d9c4309b7e7a56ee9ab25529a66471b6c08749
ShiboXing <shx26@pitt.edu>
2019-11-15 12:15:28 -0500

    created java file

--- src/PittSocial.java
+++ src/PittSocial.java
@@ -5,6 +5,8 @@ public class PittSocial {
     public static void main(String args[]) throws ClassNotFoundException
     {
         Class c=Class.forName("org.postgresql.Driver");
-        
+        Properties props =new Properties();
+        props.setProperty("user","postgres");
+        props.setProperty("password","password");
     }
 }
