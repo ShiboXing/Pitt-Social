900150a8dac42b6fe0f8a44c272af293ac2e13cc
Zhuolun1996 <zhuolun1996@gmail.com>
2019-11-15 12:56:16 -0500

    structured

--- src/PittSocial.java
+++ src/PittSocial.java
@@ -5,15 +5,15 @@ public class PittSocial {
     Connection _conn;
 
     public PittSocial(String username, String password, String url) throws ClassNotFoundException, SQLException {
-        Class c=Class.forName("org.postgresql.Driver");
-        Properties props =new Properties();
-        props.setProperty("user",username);
-        props.setProperty("password",password);
-        _conn =DriverManager.getConnection(url,props);
+        Class c = Class.forName("org.postgresql.Driver");
+        Properties props = new Properties();
+        props.setProperty("user", username);
+        props.setProperty("password", password);
+        _conn = DriverManager.getConnection(url, props);
     }
 
     public void createUser() throws SQLException {
         //PreparedStatement st=_conn.prepareStatement("call createuser('testest',\t'primis.in@placerateget.com',\t'5679',\n" +
-               // "    '1997-09-10','2019-01-17 07:35:18.000000');");
+        // "    '1997-09-10','2019-01-17 07:35:18.000000');");
     }
 }
