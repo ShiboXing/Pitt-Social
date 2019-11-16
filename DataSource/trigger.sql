2c55da7012de561aec8fc3fe69f24ec33e824015
Fangzheng Guo <gfz2000@foxmail.com>
2019-11-15 22:30:42 -0500

    a bug at confirm requests

--- DataSource/trigger.sql
+++ DataSource/trigger.sql
@@ -84,8 +84,8 @@ $$
     end;
 $$ language plpgsql;
 
-call createuser('testest',	'primis.in@placerateget.com',	'5679',
-    '1997-09-10','2019-01-17 07:35:18.000000');
+-- call createuser('testest',	'primis.in@placerateget.com',	'5679',
+--     '1997-09-10','2019-01-17 07:35:18.000000');
 
 --returnUserName
 drop function if exists returnUserName (userid int);
@@ -120,4 +120,66 @@ create or replace function login(inputEmail varchar(50), inputPassword varchar(5
             return thisUserid;
         end;
     $$ language plpgsql;
+
+--createGroup
+drop procedure if exists createGroup(thisUserId int, groupName varchar(50), groupSize int, groupDescription varchar(200));
+create or replace procedure createGroup(thisUserId int, groupName varchar(50), groupSize int, groupDescription varchar(200)) as
+$$
+    declare
+        thisGroupId int;
+    begin
+        insert into groupinfo(name, size, description) values(groupName, groupSize, groupDescription);
+        select gid into thisGroupId from groupinfo where groupinfo.name = groupname and groupinfo.size = groupsize and groupinfo.description = groupdescription;
+        insert into groupmember(gid, userid, role) values (thisgroupid, thisuserid, 'manager');
+    end;
+$$ language plpgsql;
+
+--initiateAddingGroup
+drop procedure if exists initiateAddingGroup(thisUserId int, groupId int, Addinginquiry varchar(200));
+create or replace procedure initiateAddingGroup(thisUserId int, groupId int, Addinginquiry varchar(200)) as
+$$
+    begin
+        insert into pendinggroupmember(gid, userid, message) values(groupid, thisuserid, addinginquiry);
+    end;
+$$ language plpgsql;
+
+--confirmRequests
+
+drop function if exists showAllRequests(thisuserid int);
+create or replace function showAllRequests(thisuserid int) returns table(fromid int, requesttype varchar(20), content varchar(200)) as
+    $$
+        declare
+        friendrequest pendingfriend;
+        groupmember pendinggroupmember;
+        friendcursor cursor for select * from friendrequest;
+        groupcursor cursor for select * from groupmember;
+        friendrecord friendrequest%rowtype;
+        grouprecord groupmember%rowtype;
+        whichgroup varchar(20);
+        begin
+            create temp table newfriends (fromid int, requesttype varchar(20), content varchar(200));
+            select fromid, message into friendrequest from pendingfriend where pendingfriend.toid = thisuserid;
+            select gid, userid, message into groupmember from pendinggroupmember where gid = (select gid from groupmember where groupmember.userid = thisuserid and groupmember.role = 'manager');
+           open friendcursor;
+            for friendrecord in friendcursor
+            loop
+                fetch friendcursor into friendrecord;
+                exit when not found;
+                insert into newfriends(fromid, requesttype, content) values (friendrecord.fromid, 'add friend', friendrecord.message);
+                end loop;
+            open groupcursor;
+            for grouprecord in groupcursor
+            loop
+                fetch groupcursor into grouprecord;
+                exit when not found;
+                whichgroup = 'add group'+ cast(grouprecord.gid as varchar);
+                insert into newfriends(fromid, requesttype, content) values (grouprecord.userid, whichgroup, friendrecord.message);
+                end loop;
+            return query select* from newfriends;
+        end;
+    $$ language plpgsql;
+
+select showallrequests(2);
+
+
 
